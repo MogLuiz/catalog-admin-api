5e7dcde86de08f6e942ee2c87f43bb190ff5ccf7
Luiz Henrique <luizhjramos@outlook.com>
2024-01-31 00:22:05 -0300

    test: ensure create a new category with default values

--- src/domain/__test__/category.entity.spec.ts
+++ src/domain/__test__/category.entity.spec.ts
@@ -1,5 +1,15 @@
-describe('first', () => {
-  it('first', () => {
-    expect(1).toBe(1);
+import { Category } from './../category.entity';
+
+describe('Category Unit Tests', () => {
+
+
+  it('Should create a new category with default values', () => {
+    const category = Category.create({ name: 'Movie' });
+
+    expect(category.name).toEqual('Movie');
+    expect(category.description).toBeNull();
+    expect(category.category_id).toBeUndefined();
+    expect(category.is_active).toBeTruthy();
+    expect(category.created_at).toBeInstanceOf(Date);
   })
 })

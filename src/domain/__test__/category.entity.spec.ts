6f8a8f793f472348276b49949eb7c06b88fff6a3
Luiz Henrique <luizhjramos@outlook.com>
2024-01-31 00:23:56 -0300

    test: ensure create a new category with correct data

--- src/domain/__test__/category.entity.spec.ts
+++ src/domain/__test__/category.entity.spec.ts
@@ -1,8 +1,6 @@
 import { Category } from './../category.entity';
 
 describe('Category Unit Tests', () => {
-
-
   it('Should create a new category with default values', () => {
     const category = Category.create({ name: 'Movie' });
 
@@ -11,5 +9,22 @@ describe('Category Unit Tests', () => {
     expect(category.category_id).toBeUndefined();
     expect(category.is_active).toBeTruthy();
     expect(category.created_at).toBeInstanceOf(Date);
+  })
+
+  it('Should create a new category with correct data', () => {
+    const date = new Date();
+
+    const category = new Category({
+      name: 'Movie',
+      description: 'Movies category',
+      is_active: false,
+      created_at: date
+    });
+
+    expect(category.name).toEqual('Movie');
+    expect(category.description).toEqual('Movies category');
+    expect(category.category_id).toBeUndefined();
+    expect(category.is_active).toBeFalsy();
+    expect(category.created_at).toEqual(date);
   })
 })
